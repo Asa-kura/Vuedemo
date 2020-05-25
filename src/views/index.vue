1cdc800c96785b44b5f0d44e6fcbc15117bd0ab5
Asa-kura <asa.kura@qq.com>
2020-05-25 20:52:38 +0800

    --story=1003388 --user=朝仓 使用ivewUI组件库开发demo https://www.tapd.cn/62658090/s/1004930

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,4 +1,12 @@
 <style scoped>
+    .layout {
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+
     .layout-logo {
         width: 100px;
         height: 30px;
